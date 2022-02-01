396e29c7ee411d6c88f44b42ea82e3437dfc4f3a
Mriiki <mekmun2018@gmail.com>
2022-02-02 01:26:31 +0530

    score weightage implemented

--- YebicFriender/app/src/main/java/com/example/yebicfriend_er/MainActivity.kt
+++ YebicFriender/app/src/main/java/com/example/yebicfriend_er/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.yebicfriend_er
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn1: Button = findViewById(R.id.button2)
+        var intent = Intent (this , SecondActivity::class.java)
+
+        btn1.setOnClickListener{
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
--- YebicFriender/app/src/main/java/com/example/yebicfriend_er/SecondActivity.kt
+++ YebicFriender/app/src/main/java/com/example/yebicfriend_er/SecondActivity.kt
@@ -1,11 +1,190 @@
 package com.example.yebicfriend_er
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var pointText: TextView
+    private lateinit var questionNoText: TextView
+    private lateinit var questionText: TextView
+    private lateinit var optionAText: RadioButton
+    private lateinit var optionBText: RadioButton
+    private lateinit var optionCText: RadioButton
+    private lateinit var submitButton: Button
+
+    private lateinit var questionBank: ArrayList<Question>
+    private var questionNo: Int = 0
+    private var answer: String = ""
+    private var score: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        pointText = findViewById(R.id.textView)
+        questionNoText = findViewById(R.id.textView2)
+        questionText = findViewById(R.id.textView3)
+
+        optionAText = findViewById(R.id.radioButton)
+        optionBText = findViewById(R.id.radioButton2)
+        optionCText = findViewById(R.id.radioButton3)
+
+        submitButton = findViewById(R.id.BUTTON)
+
+        questionBank = ArrayList()
+        questionNo = 0
+
+        setUpQuestions()
+        initlisteners()
+        display(questionNo);
+
     }
+
+    fun clearAll() {
+        optionAText.isChecked = false
+        optionBText.isChecked = false
+        optionCText.isChecked = false
+    }
+
+    fun display(index: Int) {
+        var questionObject = questionBank.get(index)
+        questionNoText.text = "Question" + (questionNo + 1).toString()
+        questionText.text = questionObject.question
+        optionAText.text = questionObject.optionA
+        optionBText.text = questionObject.optionB
+        optionCText.text = questionObject.optionC
+    }
+
+    fun setUpQuestions() {
+        questionBank.add(
+            Question(
+                "Mrinal's favourite colour" ,
+                "Black" ,
+                10,
+                "Yellow" ,
+                15,
+                "Turquoise",
+                5
+
+
+            )
+        )
+        questionBank.add(
+            Question(
+                "Mrinal usually goes by this name at college" ,
+                "Minnal" ,
+                5 ,
+                "Mriiki" ,
+                15 ,
+                "Mrigi" ,
+                10
+            )
+        )
+        questionBank.add(
+            Question(
+                "Mrinal loves to eat" ,
+                "Galaxy" ,
+                5 ,
+                "Pizza" ,
+                15 ,
+                "Brownie" ,
+                10
+
+            )
+        )
+        questionBank.add(
+            Question(
+                "What phrase or word does Mrinal use the most" ,
+                "yebic" ,
+                10 ,
+                "ok da" ,
+                15 ,
+                "yemezing" ,
+                5
+            )
+        )
+        questionBank.add(
+            Question(
+                "How many languages(spoken) does Mrinal know" ,
+                "4" ,
+                0 ,
+                "5" ,
+                15 ,
+                "6" ,
+                0
+
+            )
+        )
+        questionBank.add(
+            Question(
+                "Mrinal's favourite dog breed" ,
+                "Pugs" ,
+                5 ,
+                "Golden retrievers" ,
+                15 ,
+                "Beagles" ,
+                10
+
+            )
+        )
+    }
+    fun isCorrect(value : String) : Boolean {
+        return answer == value
+    }
+    fun initlisteners() {
+        optionAText.setOnClickListener {
+            clearAll()
+            optionAText.isChecked = true
+            answer = "A"
+        }
+
+        optionBText.setOnClickListener {
+            clearAll()
+            optionBText.isChecked = true
+            answer = "B"
+        }
+
+        optionCText.setOnClickListener {
+            clearAll()
+            optionCText.isChecked = true
+            answer = "C"
+
+        }
+        submitButton.setOnClickListener {
+            clearAll()
+
+            if (answer == "A"){score += questionBank.get(questionNo).AW}
+            if (answer == "B"){score += questionBank.get(questionNo).BW}
+            if (answer == "C"){score += questionBank.get(questionNo).CW}
+            pointText.text = "Points: $score"
+
+
+            if (questionNo < (questionBank.size - 1)) {
+                questionNo++
+                display(questionNo)
+
+            } else {
+                //go to next page
+                var intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("score", score.toString())
+                startActivity(intent)
+            }
+        }
+    }
+    data class  Question (
+        var  question: String ,
+        var optionA: String ,
+        var AW: Int,
+        var optionB: String ,
+        var BW: Int,
+        var optionC: String ,
+        var CW: Int,
+    )
+
+
+
 }
